5e7440838b097a46c93036e86d02bb56983b6156
PagiY <pagiyap003@gmail.com>
2023-08-29 23:36:32 +0800

    Initialized session

--- sessions-redis/server/src/index.ts
+++ sessions-redis/server/src/index.ts
@@ -1,8 +1,27 @@
 import express, { Express } from 'express';
+import session from 'express-session';
+import RedisStore from 'connect-redis';
+
+import redis from './redisdb.js';
 
 const app: Express = express();
+const redisStore = new RedisStore({
+  client: redis,
+})
 app.use(express.json());
-
+app.use(
+  session({
+    store: redisStore,
+    secret: 'test',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      secure: false, // set to true if in production to transmit cookies over https only
+      httpOnly: true, // prevent client side to view cookies
+      maxAge: 1000 * 60 * 30 // 30 minutes cookie max age
+    }
+  })
+)
 const PORT = 8000;
 app.listen(PORT, () => {
   console.log(`Server listening at PORT ${PORT}`);
