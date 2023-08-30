f444966a590218b3a26548cfb5f0d0a483fa3f1b
PagiY <pagiyap003@gmail.com>
2023-08-30 21:42:08 +0800

    Modified session object

--- sessions-redis/server/src/index.ts
+++ sessions-redis/server/src/index.ts
@@ -3,6 +3,7 @@ import session from 'express-session';
 import RedisStore from 'connect-redis';
 
 import redis from './redisdb.js';
+import auth from './routes/auth.js';
 
 const app: Express = express();
 const redisStore = new RedisStore({
@@ -15,13 +16,18 @@ app.use(
     secret: 'test',
     saveUninitialized: false,
     resave: false,
+    name: 'hello-cookie',
     cookie: {
       secure: false, // set to true if in production to transmit cookies over https only
       httpOnly: true, // prevent client side to view cookies
-      maxAge: 1000 * 60 * 30 // 30 minutes cookie max age
+      maxAge: 1000 * 60 * 30, // 30 minutes cookie max age
+      sameSite: 'lax',
     }
   })
-)
+);
+
+app.use('/api', auth);
+
 const PORT = 8000;
 app.listen(PORT, () => {
   console.log(`Server listening at PORT ${PORT}`);
